7a47fbeebc13a7317048d8eac27369b8ae8fc2bd
Netanel <mordakhay.netanel@gmail.com>
2024-10-21 15:55:47 +0300

    Added i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import "@mantine/core/styles.css";
 import { DirectionProvider, MantineProvider } from "@mantine/core";
+//import { useTranslation } from "react-i18next";
 import { theme } from "./theme";
-import Demo from "./components/Demo";
-import LanguageSwitcher from "./components/LanguageSwticher";
+
+//import Demo from "./components/Demo";
+//import LanguageSwitcher from "./components/LanguageSwticher";
 
 export default function App() {
+  //const { t } = useTranslation();
+
   return (
     <DirectionProvider>
       <MantineProvider theme={theme}>
-        <Demo />
-        <LanguageSwitcher />
+        <h1>Hello world</h1>
       </MantineProvider>
     </DirectionProvider>
   );
