4f3ff926103c95f6bb51368136b4a20a419327ae
Netanel <mordakhay.netanel@gmail.com>
2024-10-21 18:29:09 +0300

    update files

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import { Title } from "@mantine/core";
+import hatIcon from "../assets/img/hat.png";
+import "../global.css";
+
+const Logo = () => {
+  return (
+    <div>
+      <Title>GradeGuard</Title>
+    </div>
+  );
+};
+
+export default Logo;
