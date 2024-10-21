8ecb6cec45233238548460d42c89e3d2387062d7
Netanel <mordakhay.netanel@gmail.com>
2024-10-22 02:24:50 +0300

    Stepper language

--- src/pages/signup/SignupStepper.tsx
+++ src/pages/signup/SignupStepper.tsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import { Stepper } from "@mantine/core";
+import { useTranslation } from "react-i18next";
 
 const SignupStepper = () => {
+  const { t } = useTranslation();
   const [active, setActive] = useState(1);
 
   return (
     <Stepper active={active} onStepClick={setActive} orientation="vertical">
-      <Stepper.Step label="Step 1" description="Find out about GradeGuard" />
-      <Stepper.Step label="Step 2" description="Create your new account" />
-      <Stepper.Step label="Step 3" description="Verify email" />
-      <Stepper.Step label="Step 4" description="Better your grades!" />
+      <Stepper.Step
+        label={t("signup.stepper.step1")}
+        description={t("signup.stepper.step1desc")}
+      />
+      <Stepper.Step
+        label={t("signup.stepper.step2")}
+        description={t("signup.stepper.step2desc")}
+      />
+      <Stepper.Step
+        label={t("signup.stepper.step3")}
+        description={t("signup.stepper.step3desc")}
+      />
+      <Stepper.Step
+        label={t("signup.stepper.step4")}
+        description={t("signup.stepper.step4desc")}
+      />
     </Stepper>
   );
 };
