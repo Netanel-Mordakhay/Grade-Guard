ab1d92a821e2dbf09ee23af30c8829e3a829c3f1
Netanel <mordakhay.netanel@gmail.com>
2024-10-22 20:01:14 +0300

    logo fix

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,11 +4,11 @@ import hatIcon from "../assets/img/hat.webp";
 const Logo = () => {
   return (
     <Box className="logoContainer">
-      <Image src={hatIcon} className="logoHat" />
       <Title>
         <span className="logoTitle">Grade</span>
         <span className="logoTitle logoTitleBlue">Guard</span>
       </Title>
+      <Image src={hatIcon} className="logoHat" />
     </Box>
   );
 };
