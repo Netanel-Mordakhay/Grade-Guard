5e917b8b1ad4cc040fba8ece46bebf5e4d867692
Netanel <mordakhay.netanel@gmail.com>
2024-10-22 18:29:49 +0300

    Build update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "@mantine/core/styles.css";
 import { DirectionProvider, MantineProvider, Stack } from "@mantine/core";
 import { theme } from "./theme";
 import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
 import "animate.css";
 import "./styles/global.css";
 
