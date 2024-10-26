615f7e8421149de87d3159194d46f5c66a39c313
Netanel <mordakhay.netanel@gmail.com>
2024-10-26 23:10:14 +0300

    ReportBug visual update

--- src/components/navbar/ReportBug.tsx
+++ src/components/navbar/ReportBug.tsx
@@ -1,20 +1,27 @@
-import { Button, Card, Text } from "@mantine/core";
+import { Button, Divider, Group, Paper, Stack, Text } from "@mantine/core";
 import { FaBug } from "react-icons/fa";
 
 const ReportBug = () => {
   return (
-    <Card withBorder shadow="sm" radius="md" ta="center">
-      <Card.Section withBorder inheritPadding py="xs">
-        <Text>
-          <FaBug /> Found a BUG?
-        </Text>
-      </Card.Section>
-
-      <Card.Section p="lg">
-        <Text>Report it now so we'll take care of it ASAP!</Text>
-        <Button mt="sm">REPORT</Button>
-      </Card.Section>
-    </Card>
+    <Paper
+      withBorder
+      p="md"
+      radius="md"
+      className="statisticsContainer statisticsText"
+    >
+      <Stack ta="center" align="center">
+        <Group>
+          <Text fw="700">
+            <FaBug /> Found a BUG?
+          </Text>
+        </Group>
+        <Stack>
+          <Divider />
+          <Text>Report it now so we'll take care of it ASAP!</Text>
+          <Button>REPORT</Button>
+        </Stack>
+      </Stack>
+    </Paper>
   );
 };
 
